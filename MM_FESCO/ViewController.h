b02b5347ede5e2cc65e36e86dab5fa2b89fc1e86
fesco <Mortimey@workdemacbook-pro.local>
2016-07-27 15:31:45 +0800

    Initial Commit

---
+++ MM_FESCO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MM_FESCO
+//
+//  Created by Mortimey on 16/7/27.
+//  Copyright © 2016年 Mortimey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MM_FESCOTests/MM_FESCOTests.m
@@ -0,0 +1,39 @@
+//
+//  MM_FESCOTests.m
+//  MM_FESCOTests
+//
+//  Created by Mortimey on 16/7/27.
+//  Copyright © 2016年 Mortimey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MM_FESCOTests : XCTestCase
+
+@end
+
+@implementation MM_FESCOTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
