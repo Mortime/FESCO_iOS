2fa813ee9f7dbd08c7b38aae1a85e3a59ad6d95f
Mortimey <zhangyatao@zhangyataodeMacBook-Pro.local>
2017-01-12 16:25:10 +0800

    报销列表优化

---
+++ MM_FESCO/Module/Work/Reimburse/Model/ProgressReimburseModel.h
@@ -0,0 +1,13 @@
+//
+//  ProgressReimburseModel.h
+//  MM_FESCO
+//
+//  Created by Mortimey on 2017/1/12.
+//  Copyright © 2017年 Mortimey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProgressReimburseModel : NSObject
+
+@end
---
+++ MM_FESCO/Module/Work/Reimburse/Model/ProgressReimburseModel.m
@@ -0,0 +1,13 @@
+//
+//  ProgressReimburseModel.m
+//  MM_FESCO
+//
+//  Created by Mortimey on 2017/1/12.
+//  Copyright © 2017年 Mortimey. All rights reserved.
+//
+
+#import "ProgressReimburseModel.h"
+
+@implementation ProgressReimburseModel
+
+@end
