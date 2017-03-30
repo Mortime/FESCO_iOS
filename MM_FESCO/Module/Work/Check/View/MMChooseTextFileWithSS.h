d4a578bf75b16059f687c9d29c2d135426c7d13b
Mortime <Mortimey@163.com>
2017-03-30 14:14:27 +0800

    补签审批模块数据库优化

---
+++ MM_FESCO/Module/Work/Check/View/MMChooseTextFileWithSS.h
@@ -0,0 +1,13 @@
+//
+//  MMChooseTextFileWithSS.h
+//  MM_FESCO
+//
+//  Created by Mortimey on 2017/3/30.
+//  Copyright © 2017年 Mortimey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMChooseTextFileWithSS : UIView
+
+@end
---
+++ MM_FESCO/Module/Work/Check/View/MMChooseTextFileWithSS.m
@@ -0,0 +1,21 @@
+//
+//  MMChooseTextFileWithSS.m
+//  MM_FESCO
+//
+//  Created by Mortimey on 2017/3/30.
+//  Copyright © 2017年 Mortimey. All rights reserved.
+//
+
+#import "MMChooseTextFileWithSS.h"
+
+@implementation MMChooseTextFileWithSS
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
